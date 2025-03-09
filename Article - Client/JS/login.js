c145cb2c239a1592c76da45eaf25f730ebf9fec1
Riyad Murad <riyadmurad01@gmail.com>
2025-03-10 01:14:34 +0200

    Fixed Login form with axios

---
+++ Article - Client/JS/login.js
@@ -0,0 +1,22 @@
+base_api = "http://localhost/DevOps_Article/Article - Server/Apis/v1";
+
+login = async function(){
+    event.preventDefault();
+
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    // Data to send
+    const data = {
+        email: email.value,
+        password: password.value
+    };
+
+    try {
+        const response = await axios.post(base_api + "/login.php", data);
+        console.log(response.data); 
+        window.location.href = "home.html";
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
--- Article - Server/Apis/v1/login.php
+++ Article - Server/Apis/v1/login.php
@@ -8,6 +8,8 @@ User::init($conn);
 if (isset($data["email"]) && isset($data["password"])) {
     $email = $data["email"];
     $password = $data["password"];
+
+    $hashedPassword = HashingPassword($password);
 } else {
     sendResponse(false, "Missing parameters");
 }
@@ -16,7 +18,8 @@ try {
     $user = User::findByEmail($email);
 
     if ($user) {
-        if (password_verify($password, $user["password"])) {
+        // if (password_verify($hashedPassword, $user["password"])) {
+        if ($hashedPassword === $user["password"]) {
             sendResponse(true, "Login successful!");
         } else {
             sendResponse(false, "Incorrect password");
